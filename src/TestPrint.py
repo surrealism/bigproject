9f4eeaef9ea9042146040a77e7b03ce7fe88151e
surrealism <chnlcc@gmail.com>
2018-03-08 21:25:27 +0800

    timer start

--- src/TestPrint.py
+++ src/TestPrint.py
@@ -1,1 +1,10 @@
-print 'new test'
+import threading
+
+def fun_timer():
+	print('new test timer')
+	global timer
+	timer = threading.Timer(5.5, fun_timer)
+	timer.start()
+	
+timer = threading.Timer(1, fun_timer)
+timer.start()
