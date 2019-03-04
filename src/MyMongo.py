76164e3576f64427f0bacfefcbbb3400dcee66ba
surrealism <chnlcc@gmail.com>
2019-03-04 23:04:09 +0800

    my mongo 2

--- src/MyMongo.py
+++ src/MyMongo.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import datetime
 from pymongo import MongoClient
-conn = MongoClient('mongodb://surrealism:dr0675@ds121089.mlab.com/lccdb')
-db = conn.mydb  #11
-my_set = db.test_set  #22
+import logging
+from flask import request
+
+def get_request_ip():
+	'''获取请求方的ip'''
+	try:
+		ip = request.remote_addr
+		logging.error('------ ip = %s ------' % ip)
+		return ip
+	except Exception as e:
+		logging.error(e)
+
+
+def get_my_ip():
+	'''获取本机的ip'''
+	try:
+ 
+		name = socket.getfqdn(socket.gethostname())
+		ip = socket.gethostbyname(name)
+		logging.error('------ ip = %s ------' % ip)
+		return ip
+	except Exception as e:
+		logging.error(e)
+			
+conn = MongoClient('mongodb://lcc:lcc@ds121089.mlab.com:21089/lccdb')
+db = conn.lccdb  #11
+my_set = db.foo  #22
+
+my_set.insert({"d":datetime.datetime.now(),"ip":get_request_ip()})
+for i in my_set.find():
+    print(i)
