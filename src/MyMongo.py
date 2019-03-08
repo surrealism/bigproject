f67e96ccd97daf7b4426a4c2e8dca4919ecfc342
surrealism <chnlcc@gmail.com>
2019-03-08 22:52:40 +0800

    mongo test 3

--- src/MyMongo.py
+++ src/MyMongo.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import datetime
-from pymongo import MongoClient
 import logging
+import socket
+import random
 from flask import request
+from pymongo import MongoClient
 
+def Unicode():
+    val = random.randint(0x4e00, 0x9fbf)
+    return unichr(val)
+	
 def get_request_ip():
 	'''获取请求方的ip'''
 	try:
@@ -30,6 +36,6 @@ conn = MongoClient('mongodb://lcc:lcc@ds121089.mlab.com:21089/lccdb')
 db = conn.lccdb  #11
 my_set = db.foo  #22
 
-my_set.insert({"d":datetime.datetime.now(),"ip":get_request_ip()})
+my_set.insert({"d":datetime.datetime.now(),"ip":get_my_ip(),"zh":Unicode()+Unicode()+Unicode()})
 for i in my_set.find():
     print(i)
