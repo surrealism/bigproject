864dc67862541a7c842bf9a33e2c9539fb9cf302
lcc <chnlcc@gmail.com>
2023-09-10 21:59:44 +0800

    Swift first trial

---
+++ src/swift/SwTest.swift
@@ -0,0 +1,54 @@
+/* Swift 学习笔记 */
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+let minValue = UInt8.min  // minValue 为 0，是 UInt8 类型
+let maxValue = UInt8.max  // maxValue 为 255，是 UInt8 类型
+
+let decimalInteger = 17
+let binaryInteger = 0b10001       // 二进制的17
+let octalInteger = 0o21           // 八进制的17
+let hexadecimalInteger = 0x11     // 十六进制的17
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+for character in "Dog!🐶" {
+    print(character)
+}
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.count)")
+// 打印输出“the number of characters in cafe is 4”
+
+word += "\u{301}"    // 拼接一个重音，U+0301
+
+print("the number of characters in \(word) is \(word.count)")
+// 打印输出“the number of characters in café is 4”
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person:"Bob", day: "Tuesday")
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores:[5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
